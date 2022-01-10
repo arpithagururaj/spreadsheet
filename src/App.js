a6b105fa0530987275171df66274460c289d55ea
adityam945 <adityam945@gmail.com>
2022-01-10 13:58:56 +0530

    shorter function

--- src/App.js
+++ src/App.js
@@ -4,76 +4,38 @@ import React  from "react";
  import { Data } from "./data";
 
  import * as XLSX from "xlsx";
+ import * as xlsx from "xlsx";
 
 class ExcelToJson extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
     this.state = {
       file: "",
-      dataInFile: []
+      dataInFile:[]
     };
   }
 
-  handleClick(e) {
-    this.refs.fileUploader.click();
-  }
+ 
 
-  filePathset(e) {
-    e.stopPropagation();
+
+   readUploadFile = (e) => {
     e.preventDefault();
-    var file = e.target.files[0];
-    console.log(file);
-    this.setState({ file });
-
-    console.log(this.state.file);
-  }
-
-  readFile() {
-    var f = this.state.file;
-    var name = f.name;
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      // evt = on_file_select event
-      /* Parse data */
-      const bstr = evt.target.result;
-      const wb = XLSX.read(bstr, { type: "binary" });
-      /* Get first worksheet */
-      const wsname = wb.SheetNames[0];
-      const ws = wb.Sheets[wsname];
-      /* Convert array of arrays */
-      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-      /* Update state */
-      console.log("Data>>>" + data);// shows that excel data is read
-      console.log(this.convertToJson(data)); // shows data in json format
-      this.setState({
-        dataInFile: this.convertToJson(data)
-      })
-    };
-    reader.readAsBinaryString(f);
-  }
-
-  convertToJson(csv) {
-    var lines = csv.split("\n");
-
-    var result = [];
-
-    var headers = lines[0].split(",");
-
-    for (var i = 1; i < lines.length; i++) {
-      var obj = {};
-      var currentline = lines[i].split(",");
-
-      for (var j = 0; j < headers.length; j++) {
-        obj[headers[j]] = currentline[j];
-      }
-
-      result.push(obj);
+    if (e.target.files) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = xlsx.read(data, { type: "array" });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = xlsx.utils.sheet_to_json(worksheet);
+            console.log(json);
+            this.setState({
+              dataInFile: json
+            })
+        };
+        reader.readAsArrayBuffer(e.target.files[0]);
     }
-
-    return JSON.stringify(result); //JSON
-  }
-
+}
 
 
   render() {
@@ -83,16 +45,10 @@ class ExcelToJson extends React.Component {
           type="file"
           id="file"
           ref="fileUploader"
-          onChange={this.filePathset.bind(this)}
+          onChange={this.readUploadFile}
         />
-        <button
-          onClick={() => {
-            this.readFile();
-          }}
-        >
-          Read File
-        </button>
-        {this.state.dataInFile}
+       
+        {/* {JSON.stringify(this.state.dataInFile[0] )} */}
 
         <br/>
         <br/>
@@ -110,15 +66,15 @@ class ExcelToJson extends React.Component {
                  
         {/* {this.state.dataInFile[1]} */}
                     
-                {/* {this.state.dataInFile.map( info =>
+                {this.state.dataInFile.map( info =>
                  (
                   <tr>
-                  <td>{info.id}</td>
+                  <td>{info.Id}</td>
                   <td>{info.name}</td>
                   <td>{info.age}</td>
                   </tr>
                  )
-                )}                     */}
+                )}                    
                 </tbody>
             </table>
       </div>
